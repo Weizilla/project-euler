db8061d2b3b404cb0614b84c9d68c9dedf06064a
Wei Yang <wei@weizilla.com>
2014-11-23 22:34:04 -0500

    rename and improve runner

--- scala/src/main/scala/runner.scala
+++ scala/src/main/scala/runner.scala
@@ -1,14 +1,14 @@
 import scala.concurrent.duration.{SECONDS, NANOSECONDS, Duration}
 
-abstract class common {
+abstract class runner {
   def run(): Any
 
-  def time[R](block: => R): R = {
+  def time[R](block: => R, message: String = ""): R = {
     val t0: Long = System.nanoTime()
     val result = block
     val t1: Long = System.nanoTime()
     val duration = Duration(t1 - t0, NANOSECONDS)
-    println("Elapsed time: " + duration.toUnit(SECONDS) + " seconds")
+    println("Elapsed time: " + duration.toUnit(SECONDS) + " seconds " + message)
     result
   }
 
