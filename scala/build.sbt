9b5af3e7b8f461e3c3d8c3aec6fc82eb4398f153
Wei Yang <wei@weizilla.com>
2014-11-25 22:53:01 -0500

    add problem 19

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,4 +1,5 @@
 name := "project-euler"
 
 version := "1.0"
-    
+
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.6.0"
---
+++ scala/src/main/scala/problem019.scala
@@ -0,0 +1,33 @@
+import org.joda.time.{DateTimeConstants, DateTime}
+
+/** https://projecteuler.net/problem=19
+  * You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+  171
+  */
+object problem019 extends runner {
+  override def mainCase(): Long = {
+    var curr = new DateTime(1901, 1, 1, 0, 0)
+    val end = new DateTime(2000, 12, 31, 0, 0)
+    println(curr)
+    println(end)
+    var counter = 0
+    while (curr.isBefore(end)) {
+      curr = curr.plusWeeks(1).withDayOfWeek(DateTimeConstants.SUNDAY)
+      if (curr.getDayOfMonth == 1) {
+        counter += 1
+      }
+    }
+    counter
+  }
+}
