2c9d54d482dec7cd8c29e5b9ec9c894aa57290fd
Wei Yang <wei@weizilla.com>
2014-11-29 13:45:41 -0500

    problem 24 and 28

---
+++ scala/src/main/scala/problem048.scala
@@ -0,0 +1,23 @@
+/** https://projecteuler.net/problem=48
+ * The series, 1^^1 + 2^^2 + 3^^3 + ... + 10^^10 = 10405071317.
+
+Find the last ten digits of the series, 1^^1 + 2^^2 + 3^^3 + ... + 1000^^1000.
+
+Answer:
+9110846700
+ */
+object problem048 extends runner {
+  val modder = BigInt(10000000000L)
+  def calculate(limit: Int): BigInt = {
+    (1 to limit).map(selfPower).sum.mod(modder)
+  }
+
+  def selfPower(num: Int): BigInt = {
+    BigInt(num).modPow(num, modder)
+  }
+
+  override def mainCase(): Long = {
+    println(calculate(1000))
+    0
+  }
+}
