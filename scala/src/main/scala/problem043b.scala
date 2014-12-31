47792e236d4c122c34b8d6ae6774bd5624a6297e
Wei Yang <wei@weizilla.com>
2014-12-30 18:01:30 -0600

    43b

--- scala/src/main/scala/problem043b.scala
+++ scala/src/main/scala/problem043b.scala
@@ -2,13 +2,9 @@
   16695334890
   */
 object problem043b extends runner {
-  def isValid(num: String): Boolean = {
-    val divisor = Array(2, 3, 5, 7, 11, 13, 17)
-    (1 to 7).forall(i => num.substring(i, i + 3).toInt % divisor(i - 1) == 0)
-  }
-
   def calc(): Long = {
-    (0 to 9).permutations.map(i => i.mkString).filter(_.size == 10).filter(isValid).map(_.toLong).sum
+    //TODO
+    0
   }
 
   override def mainCase(): Long = {
